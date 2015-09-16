d5bbb97707faf50fceb80c76d0d8762d5575b2b1
Matthew Yang <yang70@gmail.com>
2015-09-16 15:10:56 -0700

    update db and create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+teams = Team.create([{ name: 'Seahawks', sport: 'Football'}, { name: 'Mariners', sport: 'Baseball'}, { name: 'Sounders FC', sport: 'Soccer'}, { name: 'Huskies', sport: 'Football'}])
+
+russ = ['Seahawks', 'Russel Wilson', '3', 'Quarterback']
+felix = ['Mariners', 'Felix Hernandez', '34', 'Starting Pitcher']
+clint = ['Sounders FC', 'Clint Dempsey', '2', 'Striker/Midfielder']
+jake = ['Huskies', 'Jake Browning', '3', 'Quarterback']
+
+all_players = [russ, felix, clint, jake]
+
+all_players.each do |player|
+  Team.find_by(name: player[0]).players.build({ name: player[1], number: player[2], position: player[3] }).save
+end
